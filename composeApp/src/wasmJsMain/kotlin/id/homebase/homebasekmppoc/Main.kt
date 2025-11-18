75741e3760af70ac6d28328dbd5c1fbfba90e9ed
Sebastian Bargmann <sebbarg@gmail.com>
2025-11-18 13:39:28 +0100

    wasm callback

--- composeApp/src/wasmJsMain/kotlin/id/homebase/homebasekmppoc/Main.kt
+++ composeApp/src/wasmJsMain/kotlin/id/homebase/homebasekmppoc/Main.kt
@@ -1,12 +1,28 @@
 package id.homebase.homebasekmppoc
 
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import id.homebase.homebasekmppoc.youauth.YouAuthCallbackRouter
 import kotlinx.browser.document
+import kotlinx.browser.window
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalWasmJsInterop::class)
 fun main() {
+    // Capture callback URL before rendering
+    val currentUrl = window.location.href
+    val hasCallback = currentUrl.contains("identity=") && currentUrl.contains("public_key=")
+
     ComposeViewport(document.body!!) {
+        // Process callback after UI is initialized
+        if (hasCallback) {
+            LaunchedEffect(Unit) {
+                YouAuthCallbackRouter.handleCallback(currentUrl)
+                // Clean URL to remove sensitive params from browser history
+                window.history.replaceState(null, "", window.location.pathname)
+            }
+        }
+
         App()
         MessageDialogHandler()
     }
