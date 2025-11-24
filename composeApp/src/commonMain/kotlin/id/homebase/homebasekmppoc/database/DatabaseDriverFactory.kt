3baa1bb139f4e7083a458064e7e996db36cf420c
Sebastian Bargmann <sebbarg@gmail.com>
2025-11-24 15:06:44 +0100

    cleanup

--- composeApp/src/commonMain/kotlin/id/homebase/homebasekmppoc/database/DatabaseDriverFactory.kt
+++ composeApp/src/commonMain/kotlin/id/homebase/homebasekmppoc/database/DatabaseDriverFactory.kt
@@ -2,6 +2,7 @@ package id.homebase.homebasekmppoc.database
 
 import app.cash.sqldelight.db.SqlDriver
 
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 expect class DatabaseDriverFactory {
     fun createDriver(): SqlDriver
 }
