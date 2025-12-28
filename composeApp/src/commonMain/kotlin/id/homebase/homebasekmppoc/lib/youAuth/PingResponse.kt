92fef8f07486aaac1dd42c0a99cdd535ef95d5cc
Seifert69 <michael@corleone.com>
2025-12-28 09:52:55 +0100

    Using the new ping API

---
+++ composeApp/src/commonMain/kotlin/id/homebase/homebasekmppoc/lib/youAuth/PingResponse.kt
@@ -0,0 +1,9 @@
+package id.homebase.homebasekmppoc.lib.youAuth
+
+import kotlinx.serialization.Serializable
+
+/** Response from the health/ping endpoint containing the server identity. */
+@Serializable
+data class PingResponse(
+    val identity: String
+)
